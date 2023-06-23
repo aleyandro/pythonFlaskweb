1e0ddd0b2c5a64b78762f20c7cd7bb39f8755360
Alex Zandamela <alex.zandamela@standardbank.co.za>
2023-06-23 15:12:40 +0200

    updated main.py,auth &templates

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request,flash
 
 auth = Blueprint('auth', __name__)
 
@@ -13,4 +13,24 @@ def logout():
 
 @auth.route('/sign-up', methods=(['GET','POST']))
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('email must be greater than 3 characters',category='error')
+        elif len(first_name) < 2 :
+            flash('first name must be greater than one character',category='error')
+        elif(password1!=password2): 
+            flash('Passwords dont match',category='error') 
+        elif len(password1) < 7:
+            flash('password must be atleast 7 characters')
+        else:
+            flash('Account created', category='Success')   
+
+
+
+
     return render_template("sign_up.html")
